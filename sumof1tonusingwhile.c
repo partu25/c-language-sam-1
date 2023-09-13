4d697113636f3a60861f1bc02c527e72c2c11ea4
partu25 <143474168+partu25@users.noreply.github.com>
2023-09-13 09:19:51 +0530

    sumof1tonusingwhile.c

--- sumof1tonusingwhile.c
+++ sumof1tonusingwhile.c
@@ -1,14 +1,16 @@
-
 #include<stdio.h>
+
 void main(){
+	
 	int n,i=1,sum=0;
+	 
+	printf("Enter Number : ");
+	scanf("%d",&n);
 	
+	while(i<=n){
 	
-	while(i<=3){
-		scanf("%d",&n);
-	
-		i=i+1;
-		sum = sum+n;		
+		sum=sum+i;
+		i++;		
 	}
     printf("sum is : %d",sum);
 }
